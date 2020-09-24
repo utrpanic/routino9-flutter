70233c268b75f9a1f84244c66471e9a52bde5b26
box-jeon <utrpanic@gmail.com>
2020-09-25 01:38:30 +0900

    Ch7. Add CupertinoTabScaffold.

--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,70 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import 'package:flutter/cupertino.dart';
+
+final GlobalKey<NavigatorState> firstTabNavKey = GlobalKey<NavigatorState>();
+final GlobalKey<NavigatorState> secondTabNavKey = GlobalKey<NavigatorState>();
+final GlobalKey<NavigatorState> thirdTabNavKey = GlobalKey<NavigatorState>();
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Startup Name Generator',
-      home: RandomWords(),
+    return CupertinoApp(
+      title: 'Flutter Demo',
+      theme: CupertinoThemeData(primaryColor: Colors.red),
+      home: MyHomePage(),
+      localizationsDelegates: const <LocalizationsDelegate<dynamic>>[
+        DefaultMaterialLocalizations.delegate,
+        DefaultWidgetsLocalizations.delegate,
+      ],
     );
   }
 }
 
-class RandomWords extends StatefulWidget {
+class MyHomePage extends StatefulWidget {
   @override
-  _RandomWordsState createState() => _RandomWordsState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _biggerFont = TextStyle(fontSize: 18.0);
-
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Startup Name Generator'),
+    return CupertinoTabScaffold(
+      tabBar: CupertinoTabBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            title: Text('Home'),
+            icon: Icon(Icons.home),
+          ),
+          BottomNavigationBarItem(
+            title: Text('Service'),
+            icon: Icon(Icons.assignment),
+          ),
+          BottomNavigationBarItem(
+            title: Text('My Account'),
+            icon: Icon(Icons.account_circle),
+          ),
+        ],
       ),
-      body: _buildSuggestions(),
-    );
-  }
-
-  Widget _buildSuggestions() {
-    return ListView.builder(
-        padding: EdgeInsets.all(16.0),
-        itemBuilder: /*1*/ (context, i) {
-          if (i.isOdd) return Divider(); /*2*/
-
-          final index = i ~/ 2; /*3*/
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
-          }
-          return _buildRow(_suggestions[index]);
-        });
-  }
-
-  Widget _buildRow(WordPair pair) {
-    return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: _biggerFont,
-      ),
+      tabBuilder: (context, index) {
+        if (index == 0) {
+          return CupertinoTabView(
+            navigatorKey: firstTabNavKey,
+            builder: (BuildContext context) => Container(),
+          );
+        } else if (index == 1) {
+          return CupertinoTabView(
+            navigatorKey: secondTabNavKey,
+            builder: (BuildContext context) => Container(),
+          );
+        } else {
+          return CupertinoTabView(
+            navigatorKey: thirdTabNavKey,
+            builder: (BuildContext context) => Container(),
+          );
+        }
+      },
     );
   }
 }
