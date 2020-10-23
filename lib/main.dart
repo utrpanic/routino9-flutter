d1817c5a89cdd71c71fa4584f442b3a9341ffab7
box-jeon <utrpanic@gmail.com>
2020-10-23 19:16:47 +0900

    Ch 10. 할 일 관리. Firebase 설정.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,9 +10,46 @@ class Todo {
   Todo(this.title);
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // Set default `_initialized` and `_error` state to false
+  bool _initialized = false;
+  bool _error = false;
+
+  // Define an async function to initialize FlutterFire
+  void initializeFlutterFire() async {
+    try {
+      // Wait for Firebase to initialize and set `_initialized` state to true
+      await Firebase.initializeApp();
+      setState(() {
+        _initialized = true;
+      });
+    } catch (e) {
+      // Set `_error` state to true if Firebase initialization fails
+      setState(() {
+        _error = true;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    initializeFlutterFire();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+    // if (_error) {
+    //   return SomethingWentWrong();
+    // }
+    // if (!_initialized) {
+    //   return Loading();
+    // }
     return MaterialApp(
       title: 'StopWatch',
       theme: ThemeData(
